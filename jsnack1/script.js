b526c4180038fbd954d96ced7897c683671cfef7
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 11:45:27 +0100

    commit

---
+++ jsnack1/script.js
@@ -0,0 +1,13 @@
+//inserisco 2 prompt
+
+let number1 = parseInt(prompt('numero1'));
+
+let number2 = parseInt(prompt('numero2'));
+
+if (number1 > number2) {
+    console.log(number1)
+} if (number2 > number1) {
+    console.log(number2)
+} else {
+    console.log('pareggio')
+}
