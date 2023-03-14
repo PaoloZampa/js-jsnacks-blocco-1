dd72c51fbab24f23105171f965f5d6347eabadac
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 12:13:39 +0100

    changing consolelog

--- jnack2/main.js
+++ jnack2/main.js
@@ -3,7 +3,9 @@ const word1 = prompt('inserisci parola 1');
 const word2 = prompt('inserisci parola 2');
 
 if (word1.length > word2.length) {
-    console.log(word2, word1)
+    console.log(word2);
+    console.log(word1);
 } else {
-    console.log(word1, word2)
+    console.log(word1);
+    console.log(word2);
 }
