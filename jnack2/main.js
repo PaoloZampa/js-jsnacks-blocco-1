06fe3a763ab05a09134ba6bbfbe4f9be1b7ca6fd
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 12:11:15 +0100

    commit

---
+++ jnack2/main.js
@@ -0,0 +1,9 @@
+const word1 = prompt('inserisci parola 1');
+
+const word2 = prompt('inserisci parola 2');
+
+if (word1.length > word2.length) {
+    console.log(word2, word1)
+} else {
+    console.log(word1, word2)
+}
