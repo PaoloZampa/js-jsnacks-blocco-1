1de8ae27debce3a814f698feef9bc36c2230bb63
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 11:49:45 +0100

    miglioramento

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,13 +1,15 @@
 //inserisco 2 prompt
 
-let number1 = parseInt(prompt('numero1'));
+let number1 = Number(prompt('numero1'));
 
-let number2 = parseInt(prompt('numero2'));
+let number2 = Number(prompt('numero2'));
 
 if (number1 > number2) {
     console.log(number1)
 } if (number2 > number1) {
     console.log(number2)
-} else {
+} if (number2 === number1) {
     console.log('pareggio')
+} else {
+    console.log('Devi inserire solo numeri')
 }
