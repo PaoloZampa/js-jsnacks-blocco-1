4a571e1285c11e42f8b9107362560a7914ebc5b9
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 11:56:21 +0100

    fixato nulla

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,6 +4,8 @@ let number1 = Number(prompt('numero1'));
 
 let number2 = Number(prompt('numero2'));
 
+let numbError = 'Capra, ti ho chiesto due numeri!!!'
+
 if (number1 > number2) {
     console.log(number1)
 } if (number2 > number1) {
@@ -11,5 +13,5 @@ if (number1 > number2) {
 } if (number2 === number1) {
     console.log('pareggio')
 } else {
-    console.log('Devi inserire solo numeri')
+    console.log(numbError)
 }
