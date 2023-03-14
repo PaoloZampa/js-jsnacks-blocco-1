b5d867a35bf1778f0678962de547b5552e88d107
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-14 12:41:25 +0100

    jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,15 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+const number1 = Number(prompt('inserisci un numero'));
+const number2 = Number(prompt('inserisci un numero'));
+const number3 = Number(prompt('inserisci un numero'));
+const number4 = Number(prompt('inserisci un numero'));
+const number5 = Number(prompt('inserisci un numero'));
+const number6 = Number(prompt('inserisci un numero'));
+const number7 = Number(prompt('inserisci un numero'));
+const number8 = Number(prompt('inserisci un numero'));
+const number9 = Number(prompt('inserisci un numero'));
+const number10 = Number(prompt('inserisci un numero'));
+
+console.log(`'la somma dei tuoi numeri è' ${number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10}`)
